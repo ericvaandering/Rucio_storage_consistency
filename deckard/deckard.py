c568e0f82368b7b40d5dd46767619459b1d87839
Stefan Piperov <piperov@cern.ch>
2021-08-03 15:49:01 +0200

    switch to official rucio version

--- deckard/deckard.py
+++ deckard/deckard.py
@@ -10,6 +10,7 @@ import csv
 from rucio.common import exception
 from rucio.common.types import InternalAccount, InternalScope
 from rucio.core.replica import __exists_replicas, update_replicas_states
+from rucio.core.quarantined_replica import add_quarantined_replicas
 from rucio.core.rse import get_rse_id
 from rucio.db.sqla import models
 from rucio.db.sqla.constants import (ReplicaState, BadFilesStatus)
@@ -31,65 +32,6 @@ from rucio.db.sqla.session import read_session, transactional_session
 
 from rucio.rse.rsemanager import lfns2pfns, get_rse_info, parse_pfns
 from rucio.core.rse import get_rse_protocols
-
-@transactional_session
-def add_quarantined_replicas(rse_id, replicas, session=None):
-    """
-    Bulk add quarantined file replicas.
-    :param rse_id:      The rse id.
-    :param replicas: A list of dicts with the replica information.
-    :param session:  The database session in use.
-    """
-
-    #print("rse_id: ",rse_id)
-    #print("replicas",replicas)
-    #print("len(replicas)",len(replicas))
-
-    for chunk in chunks(replicas, 100):
-        #print("chunk: ",chunk)
-        #print("len(chunk): ",len(chunk))
-
-        # Exlude files that have a registered replica.  This is a
-        # safeguard against potential issues in the Auditor.
-        file_clause = []
-
-        for replica in chunk:
-            #print("replica's scope: ", replica.get('scope', None))
-            #print("replica's name: ", replica.get('name', None))
-            #print("replica's rse_id: ", replica.get('rse_id', None))
-            #print("replica's path: ", replica.get('path', None))
-            file_clause.append(and_(models.RSEFileAssociation.scope == replica.get('scope', None),
-                                    models.RSEFileAssociation.name == replica.get('name', None),
-                                    models.RSEFileAssociation.rse_id == rse_id))
-            #print("file_clause: ",*file_clause)
-        file_query = session.query(models.RSEFileAssociation.scope,
-                                   models.RSEFileAssociation.name,
-                                   models.RSEFileAssociation.rse_id).\
-            with_hint(models.RSEFileAssociation, "index(REPLICAS REPLICAS_PK)", 'oracle').\
-            filter(or_(*file_clause))
-        #print("file_query: ",file_query)
-        existing_replicas = [(scope, name, rseid) for scope, name, rseid in file_query]
-        chunk = [replica for replica in chunk if (replica.get('scope', None), replica.get('name', None), rse_id) not in existing_replicas]
-
-        # Exclude files that have already been added to the quarantined
-        # replica table.
-        quarantine_clause = []
-        for replica in chunk:
-            quarantine_clause.append(and_(models.QuarantinedReplica.path == replica['path'],
-                                          models.QuarantinedReplica.rse_id == rse_id))
-        quarantine_query = session.query(models.QuarantinedReplica.path,
-                                         models.QuarantinedReplica.rse_id).\
-            filter(or_(*quarantine_clause))
-        quarantine_replicas = [(path, rseid) for path, rseid in quarantine_query]
-        chunk = [replica for replica in chunk if (replica['path'], rse_id) not in quarantine_replicas]
-
-        #print("chunk before insertion: ", chunk)
-        session.bulk_insert_mappings(
-            models.QuarantinedReplica,
-            [{'rse_id': rse_id, 'path': file['path'],
-              'scope': file.get('scope'), 'name': file.get('name'),
-              'bytes': file.get('bytes')} for file in chunk])
-
 
 
 
